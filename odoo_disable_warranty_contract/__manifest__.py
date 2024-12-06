b27504b79c9cf904b54fc54100006ae8cb965e33
Carlos Lopez <celm1990@gmail.com>
2024-12-06 09:11:42 -0500

    [MIG] odoo_disable_warranty_contract: migration to 18..0

--- odoo_disable_warranty_contract/__manifest__.py
+++ odoo_disable_warranty_contract/__manifest__.py
@@ -1,14 +1,16 @@
 {
     "name": "Odoo disable warranty contract",
     "summary": "Odoo disable warranty contract",
-    "version": "17.0.1.0.0",
-    "author": "Spearhead, Carlos Lopez",
+    "version": "18.0.1.0.0",
+    "author": "Spearhead, Carlos Lopez, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-brand",
     "category": "Tools",
     "license": "AGPL-3",
     "depends": [
         "base",
         "web_enterprise",
+        "disable_odoo_online",
+        "remove_odoo_enterprise",
     ],
     "assets": {
         "web.assets_backend": [
